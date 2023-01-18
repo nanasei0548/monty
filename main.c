0a2098a557e53489a006ab48b3dc6dbdac71f5d3
nanasei0548 <nanasei0548@gmail.com>
2023-01-18 20:18:03 +0000

    monty

--- main.c
+++ main.c
@@ -1,46 +1,27 @@
-#include "monty.h"
-int ERROR_MANAGE = 0;
+nclude "monty.h"
+
 /**
- * main - is the main functioj
- * @argc: int of how many arguments
- * @argv: is the arguments passed into the file
+ * main - entry point of our monty program
+ * @argc: the amount of arguments given to the program
+ * @argv: the array of arguments given to the program
  *
- * Return: if fails throught errors
+ * Return: EXIT_SUCCESS if no error has occures, otherwise EXIT_FAILURE
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	size_t size = 0;
-	FILE *monty_file;
-	char *buffer = NULL;
-	int lines = 1;
-	stack_t *stack = NULL;
-	instruction_t instruct[] = {{"push", push}, {"pall", pall},
-				    {"nop", nop}, {"pop", pop}, {"pint", pint}, {"swap", NULL},
-				    {"add", add}, {"sub", sub}, {"div", NULL}, {NULL, NULL}};
-	if (argc != 2)
+	FILE *fp;
+
+	META.error = 0;
+	META.state = STACK;
+
+	if (handle_input(argc, argv, &fp) == 1 || monty_run(fp) == 1)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		if (fp)
+			fclose(fp);
 		exit(EXIT_FAILURE);
 	}
-	monty_file = fopen(argv[1], "r");
-	if (monty_file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (getline(&buffer, &size, monty_file) > 0)
-	{
-		if (excute(&stack, lines, buffer, instruct, monty_file) == 1)
-		{
-			free(buffer);
-			free_dlist(stack);
-			fclose(monty_file);
-			exit(EXIT_FAILURE);
-		}
-		lines++;
-	}
-	free(buffer);
-	free_dlist(stack);
-	fclose(monty_file);
-	return (0);
+
+	fclose(fp);
+	return (EXIT_SUCCESS);
 }
